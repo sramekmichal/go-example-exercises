b5662f9362c7f93a76707a61e3043648a2e7b52b
sramek5 <michael.shramek@gmail.com>
2019-09-19 14:56:20 +0200

    Practicing in GO

---
+++ Exercises/Exercise - Ninja Level 3/Hands-on exercise #7.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 500
+	if x > 500 {
+		fmt.Println("Fatal Error")
+	} else if x < 500 {
+		fmt.Println("Wrong Way")
+	} else {
+		 	fmt.Println(x)
+	}
+
+
+
+}
