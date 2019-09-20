2934bb6a529e34c25126bb044d4ea423694ff6bf
sramek5 <michael.shramek@gmail.com>
2019-09-20 16:11:38 +0200

    Practicing in GO

---
+++ Exercises/Exercise - Ninja Level 4/Hands-on exercise #8.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string][]string	{
+		"bond_james":		[]string{`Shaken, not stirred`, "Martinis", "Women"},
+		"moneypenny_miss":	[]string{`James Bond`, "Literature", `Computer Science`},
+		"no_dr":			[]string{`Being evil`, `Ice cream`, "Sunsets"},
+	}
+	fmt.Println(m)
+
+	b := "\nFavourite things:"
+
+	for key, value := range m {
+		fmt.Println("\nThis is the record for: ", key, b)
+		for index, value2 := range value {
+			fmt.Println("\t\t\t", index, value2)
+		}
+
+	}
+}
