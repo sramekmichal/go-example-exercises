41707d7f8028ca05889d5d5591219bf120e00372
sramek5 <michael.shramek@gmail.com>
2019-09-24 09:45:42 +0200

    Practicing in GO

--- Exercises/Exercise - Ninja Level 5/Hands-on exercise #4.go
+++ Exercises/Exercise - Ninja Level 5/Hands-on exercise #4.go
@@ -2,12 +2,33 @@ package main
 
 import "fmt"
 
-}
 func main() {
 	a := struct {
 		first_name string
-		friends map
+		friends map[string]int
+		favourite_drinks []string
+	}{
+		first_name: "Mike",
+		friends: map[string]int{
+			"John": 55,
+			"Maribel": 28,
+			"Mr. DJ": 32,
+			"George": 46,
+		},
+		favourite_drinks: []string{
+			"Pinacolada",
+			"Water",
+			"Coke",
+		},
 	}
+	fmt.Println(a.first_name)
+	fmt.Println(a.friends)
+	fmt.Println(a.favourite_drinks)
+
+	for k, v := range a.friends {
+		fmt.Println(k, v)
 	}
-	fmt.Println(c1)
+	for k2, v2 := range a.favourite_drinks {
+		fmt.Println(k2, v2)
+	}
 }
