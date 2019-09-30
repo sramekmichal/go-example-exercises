40653a9fbd550acad22d9c6a546c4c39f0680ae7
sramek5 <michael.shramek@gmail.com>
2019-09-30 13:21:11 +0200

    Practicing in GO

---
+++ Exercises/Exercise - Ninja Level 6/Hands-on exercise #3.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	defer foo()
+	fmt.Println("Hello, world")
+}
+
+func foo() {
+	defer func() {
+		fmt.Println("foo DEFER ran")
+	}()
+	fmt.Println("foo ran")
+}
