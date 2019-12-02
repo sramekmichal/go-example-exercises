1184a685ab6d20ea4683a925fed3ba2e8486b9b7
sramek5 <michael.shramek@gmail.com>
2019-12-02 12:07:19 +0100

    Practicing in GO

---
+++ Exercises/Exercise - Ninja level 11/Hands-on exercise #2.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type person struct {
+	First   string
+	Last    string
+	Sayings []string
+}
+
+func main() {
+	p1 := person{
+		First:   "James",
+		Last:    "Bond",
+		Sayings: []string{"Shaken, not stirred", "Any last wishes?", "Never say never"},
+	}
+
+	bs, err := toJSON(p1)
+	if err != nil {
+//		fmt.Println(err)				choice 1
+//		return
+
+//		log.Println(err)				choice 2
+//		return
+
+		log.Fatalln(err)				//choice 3
+	}
+
+	fmt.Println(string(bs))
+
+}
+
+// toJSON needs to return an error also
+func toJSON(a interface{}) ([]byte, error) {
+	bs, err := json.Marshal(a)
+	if err != nil {
+		return []byte{}, fmt.Errorf("There was an error in toJSON: %v", err)
+	}
+	return bs, nil
+}
---
+++ Exercises/Exercise - Ninja level 11/description.md
@@ -0,0 +1,14 @@
+## Hands-on exercise #1
+1. Start with this code (https://play.golang.org/p/3W69TH4nON).
+2. Instead of using the blank identifier, make sure the code is checking and handling the error.
+
+**solution:** https://play.golang.org/p/tn8oiuL1Yn 
+
+## Hands-on exercise #2
+1. Start with this code (https://play.golang.org/p/9a1IAWy5E6).
+2. Create a custom error message using `fmt.Errorf`.
+
+**solutions:**
+- https://play.golang.org/p/HugU4HJEEO 
+- https://play.golang.org/p/NII-lmGasj 
+- https://play.golang.org/p/Vo5kIoR-sG 
